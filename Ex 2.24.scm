ffc0e960474d14dc2e1a2bd79518c70aadb114d0
Alex Clarke <a.clarke@runbox.com>
2016-05-05 18:40:19 +0100

    Ex 2.24 Upload Images!
    Upload images!

---
+++ Ex 2.24.scm
@@ -0,0 +1,9 @@
+#lang scheme
+
+;Give the result printed by the interpreter
+;for the expression (list 1 (list 2 (list 3 4)))
+
+;> (list 1 (list 2 (list 3 4)))
+;(1 (2 (3 4)))
+
+;Box Pointer Structure / Tree Diagram -> (See Image)
