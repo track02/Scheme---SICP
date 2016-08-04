4e6bd72a0b52dcd583db1159841409ff76836d8a
Alex Clarke <a.clarke@runbox.com>
2016-08-04 17:34:27 +0100

    Ex 2.6

--- Ex 2.60.scm
+++ Ex 2.60.scm
@@ -18,6 +18,39 @@
 ;> (element-of-set? 2 '(3 4 5 6 1 4 5 6))
 ;#f
 
+;Performance
+;This may be slower than the previous implementation (no duplicates)
+;if a set is "padded" with many duplicates before reaching the end / matching value
+;performance time will be impacted
+
+;Worst case is still the same however O(n) where the entire set must be checked, but the set size n may be larger
+
 ;adjoin-set
+;Adds an element to a set - because duplicate are allowed no checks are required
+(define (adjoin-set x set)
+  (cons x set))
+
+;Testing
+;> (adjoin-set 'x '(1 2 3 4 5))
+;(x 1 2 3 4 5)
+
+;Performance
+;This is a quicker procedure and will complete in O(1), a single operation is performed regardless of set size
+;No checks for duplicates need to be made
+
 
 ;union-set
+;If duplicates are allowed just join the two sets
+(define (union-set set1 set2)
+  (append set1 set2))
+
+
+;Testing
+;> (union-set '(1 2 3) '(1 3 4))
+;(1 2 3 1 3 4)
+
+
+;Performance here is now O(n), length of one of the sets
+;No checking is needed for each element, improving performance from the initial O(n^2)
+
+
