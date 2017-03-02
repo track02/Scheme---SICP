62f672482014bc25307225ed1588b4872e235bab
Alex Clarke <a.clarke@runbox.com>
2017-03-02 13:04:19 +0000

    Ex 3.23 - Update
    Have insertion functioning
    Trace out current deque, rear/front ptrs - determine how to implement deletion correctly

--- Ex 3.23 - Deque Implementation.scm
+++ Ex 3.23 - Deque Implementation.scm
@@ -39,6 +39,45 @@
 
 ;All operations should be accomplished in O(1) steps.
 
+;If we need to traverse the rear pointer up and down the list each item will need to link what is adjacent on either side
+;So we'll need three items
+;
+;(([x] [y]) [z])
+;   |   |    |
+;  Item |    |
+;       |    |
+;    Behind  |
+;           Front
+
+;car  --> ([x] [y])
+;caar -->  [x]
+;cadr -->      [y]
+;cdr  -->  [z]
+
+;Three steps to update the deque
+;Generate a new item and set its [y] and [z] values to point to the correct element
+;Update the structure of the deque to include the new item
+;Update the pointers to point to the new-item 
+
+;If an item is added to the rear then:
+; -> The current rear item must be updated, so it's rear points to the new item
+; -> The new item must be setup so it's front points to the current rear
+; -> the rear pointer must be moved from the current rear to the new item
+
+;Similar process if an item is added to the front:
+; -> The current front item must be updated, so it's front points to the new item
+; -> The new item must be setup so it's rear points to the current front
+; -> the front pointer must be moved from the current front to the new item
+
+;If we're deleting an item from the rear then:
+; -> Update the rear pointer to point to the front of the current rear
+; -> Null the rear of the current rear (item deleted)
+
+;If we're deleting an item from the front then:
+; -> Update the front pointer to point to the rear of the current front
+; -> Null the front of the current front (item deleted)
+
+
 (define (make-deque)
   (let ((front-ptr '())
         (rear-ptr '()))
@@ -58,24 +97,26 @@
 
     ;Insert a new item - adds an item to the back of the queue
     (define (rear-insert-deque! item)
-      (let ((new-pair (cons item '()))) ;Bundle item as a pair [x][o]
+      (let ((new-pair (cons (cons item '()) '()))) ;Bundle item (([x] [y]) [z]) 
         (cond ((empty-deque?) ;If queue is empty, then point both front and back to the item
                (set-front-ptr! new-pair)
                (set-rear-ptr!  new-pair))
               (else ;Otherwise set the cdr of the rear-ptr to the new-pair and point the rear-ptr to the item
-               (set-cdr! rear-ptr new-pair)
-               (set-rear-ptr! new-pair)))))
+               (set-cdr! (car new-pair) rear-ptr) ;Update the new item front to equal current rear
+               (set-cdr! rear-ptr new-pair) ;Update the current rear to point to the new item
+               (set-rear-ptr! new-pair))))) ;Set rear-ptr to point to the new item
 
 
     ;Insert a new item - adds an item to the front of the queue
     (define (front-insert-deque! item)
-      (let ((new-pair (cons item '()))) ;Bundle item as a pair [x][o]
+      (let ((new-pair (cons (cons item '()) '()))) ;Bundle item as a pair [x][o]
         (cond ((empty-deque?) ;If queue is empty, then point both front and back to the item
                (set-front-ptr! new-pair)
                (set-rear-ptr!  new-pair))
               (else ;Otherwise set the cdr of the new-pair to the current front-ptr and point the front-ptr to the item
-               (set-cdr! new-pair front-ptr)
-               (set-front-ptr! new-pair)))))
+               (set-cdr! (car front-ptr) new-pair) ;Update the current front to point to the new item
+               (set-cdr! new-pair front-ptr) ;Update the new item rear to point to the current front
+               (set-front-ptr! new-pair))))) ;Update the current front pointer to the new item
 
     
     
@@ -84,14 +125,15 @@
       (define (iter input output)
         (if (null? input)
             output
-            (iter (cdr input) (append (list (car input)) output))))
+            (iter (cdr input) (append (list (car (car input))) output)))) ;Attach element to start of list
       (iter front-ptr '()))
 
     ;Delete item - removes an item from the front
     (define (front-delete-deque!)
       (cond ((empty-deque?)
              (error "DELETE! called with an empty queue"))
-            (else (set-front-ptr! (cdr front-ptr)))))
+            (else (set-front-ptr! (cdr (cdr front-ptr))) ;Set the front pointer to the rear of the current front 
+                  (set-car! front-ptr '())))) ;Set the front of current front to null
 
     ;Delete item - removes an item from the back
     (define (rear-delete-deque!)
