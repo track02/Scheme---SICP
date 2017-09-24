22bee243825adabef21ad5b367484498c3141055
Alex Clarke <a.clarke@runbox.com>
2017-09-24 11:52:20 +0100

    Tables

--- Ex 3.25 - Tables.scm
+++ Ex 3.25 - Tables.scm
@@ -1,6 +1,6 @@
 #lang sicp
 
-;Exercise 3.25 - Generalizing one- and two-dimensional tables,
+;Exercise 3.25 - Generalizing one and two-dimensional tables,
 ;show how to implement a table in which values are
 ;stored under an arbitrary number of keys and different values
 ;may be stored under different numbers of keys. e
