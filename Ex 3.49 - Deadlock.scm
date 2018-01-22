3844597028e044ed6fc6a328f5fe79ee367da11a
Alex Clarke <a.clarke@runbox.com>
2018-01-22 08:50:23 +0000

    Ex 3.49

---
+++ Ex 3.49 - Deadlock.scm
@@ -0,0 +1,16 @@
+#lang scheme
+
+; Exercise 3.49:
+
+; Give a scenario where the deadlock-avoidance
+; mechanism described above does not work.
+
+; (Hint: In the exchange problem, each process knows
+; in advance which accounts it will need to get access to.
+; Consider a situation where a process must get access
+; to some shared resources before it can know which additional
+; shared resources it will require.)
+
+; Acquiring resources in order of lowest-id to highest will not be suitable
+; if we only know the resources to be modified once acquiring the lock
+; E.g. - Database modifications
